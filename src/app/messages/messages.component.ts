6a75a74e69f1a7106ce508a81f5d4a39d2676eaf
Šárka Flídrová <sarka.flidrova@email.cz>
2025-07-31 10:17:19 +0200

    add change detection strategy - OnPush

--- src/app/messages/messages.component.ts
+++ src/app/messages/messages.component.ts
@@ -1,21 +1,22 @@
-import { Component, signal } from '@angular/core';
+import { ChangeDetectionStrategy, Component, signal } from "@angular/core";
 
-import { MessagesListComponent } from './messages-list/messages-list.component';
-import { NewMessageComponent } from './new-message/new-message.component';
+import { MessagesListComponent } from "./messages-list/messages-list.component";
+import { NewMessageComponent } from "./new-message/new-message.component";
 
 @Component({
-  selector: 'app-messages',
+  selector: "app-messages",
   standalone: true,
-  templateUrl: './messages.component.html',
-  styleUrl: './messages.component.css',
+  templateUrl: "./messages.component.html",
+  styleUrl: "./messages.component.css",
   imports: [MessagesListComponent, NewMessageComponent],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class MessagesComponent {
   messages = signal<string[]>([]);
 
   get debugOutput() {
     console.log('[Messages] "debugOutput" binding re-evaluated.');
-    return 'Messages Component Debug Output';
+    return "Messages Component Debug Output";
   }
 
   onAddMessage(message: string) {
--- src/app/messages/new-message/new-message.component.ts
+++ src/app/messages/new-message/new-message.component.ts
@@ -1,24 +1,30 @@
-import { Component, output, signal } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  output,
+  signal,
+} from "@angular/core";
+import { FormsModule } from "@angular/forms";
 
 @Component({
-  selector: 'app-new-message',
+  selector: "app-new-message",
   standalone: true,
   imports: [FormsModule],
-  templateUrl: './new-message.component.html',
-  styleUrl: './new-message.component.css',
+  templateUrl: "./new-message.component.html",
+  styleUrl: "./new-message.component.css",
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class NewMessageComponent {
   add = output<string>();
-  enteredText = signal('');
+  enteredText = signal("");
 
   get debugOutput() {
     console.log('[NewMessage] "debugOutput" binding re-evaluated.');
-    return 'NewMessage Component Debug Output';
+    return "NewMessage Component Debug Output";
   }
 
   onSubmit() {
     this.add.emit(this.enteredText());
-    this.enteredText.set('');
+    this.enteredText.set("");
   }
 }
